f77686199f906c640c941ac1a69f7141305b42b4
Lata <amelatbay@gmail.com>
2025-03-20 21:41:29 +0300

    bug fix

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -5,7 +5,7 @@ def fizzbuzz(n):
     result = []
 
     for i in range(1, n + 1):
-        if i % 15 == 0:
+        if i % 5 == 0 and i % 3 == 0:
             result.append("Fizz Buzz")
         if i % 3 == 0:
             result.append("Fizz")
