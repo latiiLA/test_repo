31ecd003f40b4eb0337117b0dfe5fccb32423281
Lata <amelatbay@gmail.com>
2025-03-20 21:02:09 +0300

    fizzbuzz function added

---
+++ fizzbuzz.py
@@ -0,0 +1,21 @@
+def fizzbuzz(n):
+    '''
+    Fizz buzz function to generate the fizzbuzz for numbers from 0 to n
+    '''
+    result = []
+
+    for i in range(1, n + 1):
+        if i % 15 == 0:
+            result.append("Fizz Buzz")
+        if i % 3 == 0:
+            result.append("Fizz")
+        elif i % 5 == 0:
+            result.append("Buzz")
+        else:
+            result.append(i)
+
+
+if __name__ == '__main__':
+    n = 100
+    output = fizzbuzz(n)
+    print(output)
